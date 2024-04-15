926eead82add7ad2a2d6e929afefee9e71594e5e
Yevgen <osadchyi15@gmail.com>
2024-04-15 23:31:23 +0200

    initial

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,31 @@
 'use strict';
 
-function slugify(title) {
-        return ((title.split(" ")).join("-")).toLowerCase();;
-}
+function isEnoughCapacity(products, containerSize) {
+        let quantityOfProducts = 0;
+     
+        for (let i = 0; i < Object.values(products).length; i++) {
+                quantityOfProducts += Object.values(products)[i];
+        }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+        if (quantityOfProducts <= containerSize) {
+                return true;
+        } else {
+                return false;
+        }
+};
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
